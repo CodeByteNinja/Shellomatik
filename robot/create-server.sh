700914bfb9ac4d2e413973852a2d608b2c20bb82
Sharma Vikram <44596863+sharma-vikram@users.noreply.github.com>
2022-11-11 11:47:08 +0530

    Update create-server.sh

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi 
 
 COMPONENT=$1
-ZONE_ID="Z09253484I9PUXKN8AW8"
+ZONE_ID="Z0780003140KRETRL7AZN"
 
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
